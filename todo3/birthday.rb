eb319ea7379936ab252ae251ebbb578db97748d9
Chryus <chris.haack@me.com>
2013-11-14 09:25:26 -0500

    delete one equal sign from ternary operator to make code work

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,5 +1,5 @@
-require Date
-
-print "heya, what's your birthday?"
+print "heya, what's your birthday? "
 birthday = gets.chomp
-puts birthday == Date.today ? "Happy Birthday!" : "Have a lovely day."
+puts birthday = Date.today ? "Happy Birthday!" : "Have a lovely day."
+
+puts Date.today
