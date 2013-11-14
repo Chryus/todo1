c7fd81ea6fa0d10a74f127787d78ccd8d5d92818
Chryus <chris.haack@me.com>
2013-11-14 12:33:33 -0500

    remove quotation marks form 'i'

--- Loops/fizzbuzz/fizzbuzz.rb
+++ Loops/fizzbuzz/fizzbuzz.rb
@@ -2,16 +2,17 @@
 #when the number is divisible by 3, say fizz
 #when the number is divisible by 5 say buzz
 #when the number is divisible by 3 and 5 say fizzbuzz
+
 i = 1
 while i < 101
 	if i % 3 == 0 
 		puts "fizz"
 	elsif i % 5 == 0
 		puts "buzz"
-	elsif i % 3 && i % 5 == 0 
+	elsif i % 3 == 0 && i % 5 == 0 
 		puts "fizzbuzz"
 	else
-		puts "i"
+		puts i
 	end
 	i = i + 1
 end
