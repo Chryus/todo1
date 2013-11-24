b3237829957b93dd7efbf122bc35290a0fe4b2cd
Chris <chris.haack@me.com>
2013-11-24 12:55:02 -0500

    add notes to program

--- programming-languages/programming-languages.rb
+++ programming-languages/programming-languages.rb
@@ -1,6 +1,5 @@
-Organizing Nested Data
-
-I have a collection of Programming Languages.  
+require "awesome_print"
+# I have a collection of Programming Languages.  
 languages = {
   :oo => {
     :ruby => {
@@ -35,19 +34,19 @@ languages = {
 
 new_languages = {}
 languages.each do |oo_or_func, language_hash|
-  #:oo, {:ruby => {...}}
+  # <:oo, {:ruby => {...}}>
   language_hash.each do |language, type_hash|
-    #:ruby => {:type => "interpreted"}
-      type_hash.each do |type, str_value|
+    #<:ruby => {:type => "interpreted"}>
+      type_hash.each do |type, value|
         #:type => "interpreted"
         #if language doesn't already exist, create language with hash; this is unecessary here but is good practice
-      #if new_languages[language].nil? 
+        #if new_languages[language].nil? 
         new_languages[language] = {}
-      #end 
+        #end 
       #creates a key called style and sets it = to an array filled with oo_or_func
       new_languages[language][:style] = [oo_or_func]
       #creates a key called  type and sets is equal to string value
-      new_languages[language][:type] = str_value 
+      new_languages[language][:type] = value 
     end
   end
 end
@@ -55,16 +54,16 @@ ap new_languages
 
 #from hash > hash > hash
 #to hash > hash |key, array|
-I would like you to reformat the nested structure to be of the following form
-new_languages =
-{
-  :ruby => {
-    :style => [:oo],
-    :type => "interpreted"
-  },
-  :python => {
-    :style => [:oo],
-    :type => "interpreted"
-  }
-  etc etc
-}
+# I would like you to reformat the nested structure to be of the following form
+# new_languages =
+# {
+#   :ruby => {
+#     :style => [:oo],
+#     :type => "interpreted"
+#   },
+#   :python => {
+#     :style => [:oo],
+#     :type => "interpreted"
+#   }
+#   etc etc
+# }
