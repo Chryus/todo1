0a02eae74fe454e1408a316976a79dbf79dcb89d
Chris <chris.haack@me.com>
2013-11-26 09:40:06 -0500

    add file and create method to pass tests

---
+++ todo11/key for min value.rb
@@ -0,0 +1,11 @@
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(hash)
+ hash.each do |key, value|
+ 	if key < key
+ 		puts key
+end
+
+hash = {:blake => 500, :ashley => 2, :adam => 1})
+
+#return :adam
---
+++ todo11/key-for-min-value.rb
@@ -0,0 +1,17 @@
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+require 'awesome_print'
+
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+def key_for_min_value(hash)
+	array = []
+ 	hash.each do |key, value|
+	 	array << value <=> value	
+	end
+	array.sort!
+	hash.key(array[0])
+end
+
+key_for_min_value(hash)
+
