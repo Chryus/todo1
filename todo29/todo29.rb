1bb3ef5846f20dca03a4647583dad2b59b684851
Chris <chris.haack@me.com>
2014-01-06 10:21:18 -0500

    refactor code

--- todo29/todo29.rb
+++ todo29/todo29.rb
@@ -2,13 +2,18 @@
 # Each with object works much like inject. Use each_with_object to return an array containing one reversed string for each string that has an even number of characters.
  
 # Code:
-def even_sum(arr)
-	  temp = arr.each_with_object([]) do |str, array| 
-	  if str.length.even?
-	  	array << str.reverse
-	  end
-	end
-	return temp
-end
+# def even_sum(arr)
+# 	  arr.each_with_object([]) do |str, array| 
+# 	  if str.length.even?
+# 	  	array << str.reverse
+# 	  end
+# 	end
+# end
  
-even_sum(["cat", "dog", "bird", "fish"]) #=> ["drib", "hsif"]
+#refactor  
+def even_sum(arr) 
+	arr.each_with_object([]) {|str, array| array << str.reverse if str.length.even? }
+end
+
+puts even_sum(["cat", "dog", "bird", "fish"]) #=> ["drib", "hsif"]
+
