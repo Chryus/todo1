798a95a7ccb36a6a3d4d36dfb9c0df8ba604e5c0
Chris <chris.haack@me.com>
2014-01-08 09:30:17 -0500

    add brainstorm about tools

--- todo31/todo31.rb
+++ todo31/todo31.rb
@@ -1,7 +1,14 @@
 A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
  
-def longest_palindrome
+def longest_palindrome(string)
+	if string.reverse
  
 end
  
 longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd") #=> "dhfdkjfffhhfffjkdfhd"
+
+possible tools 
+
+regex
+string.reverse
+string.split
