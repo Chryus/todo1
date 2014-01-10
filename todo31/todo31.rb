507927afa1a4a74a1d6e71ecf4dba764574d9a3c
Chris <chris.haack@me.com>
2014-01-10 09:12:08 -0500

    add todo 32

--- todo31/todo31.rb
+++ todo31/todo31.rb
@@ -1,14 +1,22 @@
-A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
+#A palindrome is a string that is written the same forward as it is in reverse. Write a method to return the longest palindrome in a given string
  
 def longest_palindrome(string)
-	if string.reverse
- 
+	array = string.split(//)
+	palin = []
+	temp = []
+	array.each do |char|
+		string[0]
 end
  
 longest_palindrome("afbbbfjdjklgdfdhfdkjfffhhfffjkdfhdhkyejejfjkd") #=> "dhfdkjfffhhfffjkdfhd"
 
-possible tools 
+# possible tools:
 
-regex
-string.reverse
-string.split
+# regex
+# string.reverse
+# string.split
+# map
+# each with index?
+
+#if the first char matches
+
