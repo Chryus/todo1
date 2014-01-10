741cf0b8173ceb9c836774107b99fa4beaf87540
Chris <chris.haack@me.com>
2014-01-10 09:33:51 -0500

    solve todo!

--- todo32/todo32.rb
+++ todo32/todo32.rb
@@ -1,12 +1,17 @@
-Get keys of a hash whose values equal to given arguments.
+#Get keys of a hash whose values equal to given arguments.
  
-Code:
+require 'ap'
+# Code:
 class Hash
   def keys_of(*args)
-    #code goes here
+    array = []
+    args.each do |arg|
+      array << self.key(arg)
+    end
+    ap array
   end
 end
  
-{a: 1, b: 2, c: 3}.keys_of(1) #=> [:a]
-{a: 1, b: 2, c: 3, d: 1}.keys_of(1) #=>  [:a, :d]
-{a: 1, b: 2, c: 3, d: 1}.keys_of(1, 2) #=> [:a, :b, :d]
+# {a: 1, b: 2, c: 3}.keys_of(1) #=> [:a]
+# {a: 1, b: 2, c: 3, d: 1}.keys_of(1) #=>  [:a, :d]
+# {a: 1, b: 2, c: 3, d: 1}.keys_of(1, 2) #=> [:a, :b, :d]
